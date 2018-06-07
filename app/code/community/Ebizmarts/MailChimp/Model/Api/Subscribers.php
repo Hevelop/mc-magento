5a0cb99d891edd3b0437f385ba431914cb46e592
Nicolò Dian <nicolo@hevelop.com>
2018-06-07 16:28:13 +0200

    fix subscriber store id if is not a customer

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Subscribers.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Subscribers.php
@@ -287,7 +287,9 @@ class Ebizmarts_MailChimp_Model_Api_Subscribers
                             }
                             break;
                         case 'language':
-                            $storeId = $customer->getStoreId();
+                            $customerStoreId = ($customer->getStoreId() == 0) ? 1 : $customer->getStoreId();
+                            $subscriberStoreId = ($subscriber->getStoreId() == 0) ? 1 : $subscriber->getStoreId();
+                            $storeId = (isset($customerStoreId)) ? $customerStoreId : $subscriberStoreId;
                             $language = Mage::getModel('core/store')->load($storeId)->getName();
                             $eventValue = $mergeVars[$key] = $language != NULL ? $language : 'Italiano';
                             break;
